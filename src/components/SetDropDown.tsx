f44a72da45a8b3485ca930573d8fd28f1c4feca0
Rob_Versluis <henk@msn.com>
2020-08-16 00:46:31 +1000

    autocomplete for setlist

--- src/components/SetDropDown.tsx
+++ src/components/SetDropDown.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { CircularProgress } from '@material-ui/core';
 
 interface SetDescription {
-  setAbv: string;
-  setFullName: string;
+  code: string;
+  name: string;
 }
 
 export const SetDropDown = () => {
@@ -10,15 +13,39 @@ export const SetDropDown = () => {
     undefined,
   );
 
-  const requestUrl = 'https://mtgjson.com/api/v5/AllPrintings.json';
-  const request = new XMLHttpRequest();
-  request.open('GET', requestUrl);
-  request.responseType = 'json';
-  request.send();
-  request.onload = () => {
-    const data = request.response;
-    console.log(data);
-  };
-
-  return <div>drop</div>;
+  if (!setList) {
+    const requestUrl = 'https://mtgjson.com/api/v5/AllPrintings.json';
+    const request = new XMLHttpRequest();
+    request.open('GET', requestUrl);
+    request.responseType = 'json';
+    request.send();
+    request.onload = () => {
+      const data = request.response;
+      setSetList(
+        Object.keys(data?.data)?.map((set) => {
+          return {
+            code: data.data[set].code,
+            name: data.data[set].name,
+          };
+        }),
+      );
+    };
+  }
+  return (
+    <div>
+      {setList ? (
+        <Autocomplete
+          id="set-list-combo"
+          options={setList}
+          getOptionLabel={(set: SetDescription) => set.name}
+          style={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Set List" variant="outlined" />
+          )}
+        />
+      ) : (
+        <CircularProgress />
+      )}
+    </div>
+  );
 };
