371e3950e6ba158b34172adb0ddc1adb060acee8
Rob_Versluis <henk@msn.com>
2020-08-15 23:59:15 +1000

    simple xmlhttprequest to get json data

--- src/components/SetDropDown.tsx
+++ src/components/SetDropDown.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 
+interface SetDescription {
+  setAbv: string;
+  setFullName: string;
+}
+
 export const SetDropDown = () => {
-  fetch('https://mtgjson.com/api/v5/set', {
-    method: 'GET',
-    cache: 'no-cache',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    },
-  })
-    .then((res) => {
-      res.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log('error fetching data');
-    });
+  const [setList, setSetList] = React.useState<SetDescription[] | undefined>(
+    undefined,
+  );
+
+  const requestUrl = 'https://mtgjson.com/api/v5/AllPrintings.json';
+  const request = new XMLHttpRequest();
+  request.open('GET', requestUrl);
+  request.responseType = 'json';
+  request.send();
+  request.onload = () => {
+    const data = request.response;
+    console.log(data);
+  };
 
   return <div>drop</div>;
 };
