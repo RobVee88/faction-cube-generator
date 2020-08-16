40e554d13aa6a1532db7f2f6beabbd768fafd74a
Rob Versluis <versluis.rob@hotmail.com>
2020-08-16 17:21:23 +1000

    convert cube to cockatrice formatted string

--- src/components/CubeSelector.tsx
+++ src/components/CubeSelector.tsx
@@ -1,10 +1,18 @@
 import { fetchApiData } from '@/util/api';
-import { Button } from '@material-ui/core';
+import { Button, TextareaAutosize } from '@material-ui/core';
 import React from 'react';
 import { SetDropDown } from './SetDropDown';
 import { SelectedSetList } from './SelectedSetList';
-import { SetDescription, RarityDistribution } from '@/util/types';
+import { SetDescription, RarityDistribution, Card } from '@/util/types';
 import { generateCube } from '@/util/cube';
+
+const convertCubeToCockatriceFormatting = (cube: Card[]) => {
+  let cockatriceFormattedString = '';
+  cube.forEach((card) => {
+    cockatriceFormattedString = cockatriceFormattedString + card.name + '\n';
+  });
+  return cockatriceFormattedString;
+};
 
 export const CubeSelector = () => {
   const [setList, setSetList] = React.useState<SetDescription[] | undefined>(
@@ -19,6 +27,8 @@ export const CubeSelector = () => {
   >({ rares: 1, uncommons: 4, commons: 10 });
   const [cubeSize, setCubeSize] = React.useState<number>(360);
 
+  const [cube, setCube] = React.useState<Card[]>([]);
+
   if (!setList) {
     fetchApiData().then((data: SetDescription[]) => {
       setSetList(data);
@@ -26,45 +36,55 @@ export const CubeSelector = () => {
   }
 
   return (
-    <div style={{ display: 'flex' }}>
-      <div>
-        <div style={{ display: 'flex' }}>
-          <SetDropDown
-            setList={setList}
-            setSelectedSet={setSelectedSet}
-            selectedSet={selectedSet}
-          />
+    <div>
+      <div style={{ display: 'flex' }}>
+        <div>
+          <div style={{ display: 'flex' }}>
+            <SetDropDown
+              setList={setList}
+              setSelectedSet={setSelectedSet}
+              selectedSet={selectedSet}
+            />
+            <Button
+              onClick={() => {
+                if (selectedSet) {
+                  setSelectedSets((previous) => [...previous, selectedSet]);
+                  setSetList(
+                    setList.filter((set) => set.code !== selectedSet.code),
+                  );
+                  setSelectedSet(undefined);
+                }
+              }}
+            >
+              Add Set
+            </Button>
+          </div>
           <Button
-            onClick={() => {
-              if (selectedSet) {
-                setSelectedSets((previous) => [...previous, selectedSet]);
-                setSetList(
-                  setList.filter((set) => set.code !== selectedSet.code),
-                );
-                setSelectedSet(undefined);
-              }
-            }}
+            disabled={!selectedSets.length}
+            onClick={() =>
+              setCube(generateCube(selectedSets, cubeSize, rarityDistribution))
+            }
           >
-            Add Set
+            Generate Cube
           </Button>
         </div>
-        <Button
-          disabled={!selectedSets.length}
-          onClick={() =>
-            console.log(
-              generateCube(selectedSets, cubeSize, rarityDistribution),
-            )
-          }
-        >
-          Generate Cube
-        </Button>
-      </div>
 
-      <SelectedSetList
-        selectedSets={selectedSets}
-        setSelectedSets={setSelectedSets}
-        setSetList={setSetList}
-      />
+        <SelectedSetList
+          selectedSets={selectedSets}
+          setSelectedSets={setSelectedSets}
+          setSetList={setSetList}
+        />
+      </div>
+      {cube.length ? (
+        <TextareaAutosize
+          rowsMin={15}
+          style={{ width: '100%', resize: 'none' }}
+          placeholder="Generate a cube to display cards here"
+          value={convertCubeToCockatriceFormatting(cube)}
+        />
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
