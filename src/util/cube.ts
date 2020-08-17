b016d35964f24ce3ae1e975dbd85520dd8875089
Rob Versluis <versluis.rob@hotmail.com>
2020-08-17 12:50:07 +1000

    added color balancing

--- src/util/cube.ts
+++ src/util/cube.ts
@@ -1,11 +1,10 @@
 import { SetDescription, RarityDistribution, Card, Rarity } from './types';
 
 const getRandomCards = (cards: Card[], amount: number) => {
-  const result: Card[] = new Array(amount);
   let len = cards.length;
+  if (amount > len) amount = len;
+  const result: Card[] = new Array(amount);
   const taken = new Array(len);
-  if (amount > len)
-    throw new RangeError('getRandomCards: more cards taken than available');
   while (amount--) {
     const x = Math.floor(Math.random() * len);
     result[amount] = cards[x in taken ? taken[x] : x];
@@ -14,7 +13,31 @@ const getRandomCards = (cards: Card[], amount: number) => {
   return result;
 };
 
-// possible add 'unique' parameter?
+const getColorDistribution = (set: SetDescription) => {
+  let colorIdentityStats = {};
+  set.cards.forEach((card) => {
+    if (!card.supertypes.find((type) => type === 'Basic')) {
+      if (card.type === 'Land') {
+        if (colorIdentityStats.hasOwnProperty('land')) {
+          colorIdentityStats['land'] = colorIdentityStats['land'] + 1;
+        } else {
+          colorIdentityStats['land'] = 1;
+        }
+      } else if (
+        colorIdentityStats.hasOwnProperty(card.colorIdentity.join(''))
+      ) {
+        colorIdentityStats[`${card.colorIdentity.join('')}`] =
+          colorIdentityStats[`${card.colorIdentity.join('')}`] + 1;
+      } else {
+        colorIdentityStats[`${card.colorIdentity.join('')}`] = 1;
+      }
+    }
+  });
+  return colorIdentityStats;
+};
+
+// add 'unique' parameter
+// sort 'remaining' per color as well
 export const generateCube = (
   selectedSets: SetDescription[],
   cubeSize: number,
@@ -24,46 +47,83 @@ export const generateCube = (
 
   let remainder = cubeSize % selectedSets.length;
   const cardsPerSet = cubeSize / selectedSets.length;
-  const numberOfRares = Math.floor(
-    (cardsPerSet / 15) * rarityDistribution.rares,
-  );
-  const numberOfUnCommons = Math.floor(
-    (cardsPerSet / 15) * rarityDistribution.uncommons,
-  );
-  const numberOfCommons = Math.floor(
-    (cardsPerSet / 15) * rarityDistribution.commons,
-  );
 
   selectedSets.forEach((set) => {
-    cube = [
-      ...cube,
-      ...getRandomCards(
-        set.cards.filter(
-          (card) =>
-            card.rarity === Rarity.rare || card.rarity === Rarity.mythic,
+    const colorDistribution = getColorDistribution(set);
+
+    Object.keys(colorDistribution).forEach((colorIdentity) => {
+      const numberOfCardsByColor =
+        (colorDistribution[colorIdentity] / set.cards.length) * cardsPerSet;
+      const numberOfRaresByColor = Math.ceil(
+        (numberOfCardsByColor / 15) * rarityDistribution.rares,
+      );
+      const numberOfUncommonsByColor = Math.ceil(
+        (numberOfCardsByColor / 15) * rarityDistribution.uncommons,
+      );
+      const numberOfCommonsByColor = Math.ceil(
+        (numberOfCardsByColor / 15) * rarityDistribution.commons,
+      );
+      cube = [
+        ...cube,
+        ...getRandomCards(
+          set.cards.filter((card) => {
+            if (colorIdentity === 'land') {
+              return (
+                card.type === 'Land' &&
+                (card.rarity === Rarity.rare || card.rarity === Rarity.mythic)
+              );
+            } else {
+              return (
+                card.colorIdentity.join('') === colorIdentity &&
+                (card.rarity === Rarity.rare || card.rarity === Rarity.mythic)
+              );
+            }
+          }),
+          numberOfRaresByColor,
         ),
-        numberOfRares,
-      ),
-    ];
-    cube = [
-      ...cube,
-      ...getRandomCards(
-        set.cards.filter((card) => card.rarity === Rarity.uncommon),
-        numberOfUnCommons,
-      ),
-    ];
-    let finalNumberOfCommons = numberOfCommons;
-    if (remainder > 0) {
-      finalNumberOfCommons++;
-      remainder--;
-    }
-    cube = [
-      ...cube,
-      ...getRandomCards(
-        set.cards.filter((card) => card.rarity === Rarity.common),
-        finalNumberOfCommons,
-      ),
-    ];
+      ];
+      cube = [
+        ...cube,
+        ...getRandomCards(
+          set.cards.filter((card) => {
+            if (colorIdentity === 'land') {
+              return card.type === 'Land' && card.rarity === Rarity.uncommon;
+            } else {
+              return (
+                card.colorIdentity.join('') === colorIdentity &&
+                card.rarity === Rarity.uncommon
+              );
+            }
+          }),
+          numberOfUncommonsByColor,
+        ),
+      ];
+      let finalNumberOfCommonsByColor = numberOfCommonsByColor;
+      if (remainder > 0) {
+        finalNumberOfCommonsByColor++;
+        remainder--;
+      }
+      cube = [
+        ...cube,
+        ...getRandomCards(
+          set.cards.filter((card) => {
+            if (colorIdentity === 'land') {
+              return (
+                card.type === 'Land' &&
+                !card.supertypes.find((type) => type === 'Basic') &&
+                card.rarity === Rarity.common
+              );
+            } else if (!card.supertypes.find((type) => type === 'Basic')) {
+              return (
+                card.colorIdentity.join('') === colorIdentity &&
+                card.rarity === Rarity.common
+              );
+            }
+          }),
+          finalNumberOfCommonsByColor,
+        ),
+      ];
+    });
   });
 
   return cube;
