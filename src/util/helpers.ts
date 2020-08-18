0578b3b1316217214c43cbe19bdfa8125562c491
Rob Versluis <versluis.rob@hotmail.com>
2020-08-18 21:28:08 +1000

    moved some functions to helpers

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,4 +1,4 @@
-import { Card } from './types';
+import { Card, SetDescription } from './types';
 
 export const convertCubeToCockatriceFormatting = (cube: Card[]) => {
   let cockatriceFormattedString = '';
@@ -6,4 +6,40 @@ export const convertCubeToCockatriceFormatting = (cube: Card[]) => {
     cockatriceFormattedString = cockatriceFormattedString + card.name + '\n';
   });
   return cockatriceFormattedString;
+};
+
+export const getRandomCards = (cards: Card[], amount: number) => {
+  let len = cards.length;
+  if (amount > len) amount = len;
+  const result: Card[] = new Array(amount);
+  const taken = new Array(len);
+  while (amount--) {
+    const x = Math.floor(Math.random() * len);
+    result[amount] = cards[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+};
+
+export const getColorDistribution = (set: SetDescription) => {
+  let colorIdentityStats = {};
+  set.cards.forEach((card) => {
+    if (!card.supertypes.find((type) => type === 'Basic')) {
+      if (card.type === 'Land') {
+        if (colorIdentityStats.hasOwnProperty('land')) {
+          colorIdentityStats['land'] = colorIdentityStats['land'] + 1;
+        } else {
+          colorIdentityStats['land'] = 1;
+        }
+      } else if (
+        colorIdentityStats.hasOwnProperty(card.colorIdentity.join(''))
+      ) {
+        colorIdentityStats[`${card.colorIdentity.join('')}`] =
+          colorIdentityStats[`${card.colorIdentity.join('')}`] + 1;
+      } else {
+        colorIdentityStats[`${card.colorIdentity.join('')}`] = 1;
+      }
+    }
+  });
+  return colorIdentityStats;
 };
